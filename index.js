c04329b38ecaebb17b43db7af606e9c30525fd11
Rexxyflexxy2008 <143276795+Rexxyflexxy2008@users.noreply.github.com>
2023-08-26 16:06:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+    var typed= new Typed (".text",{
+strings:["Youtuber", "Web-developer", "frontend-developer","partial graphic designer"],
+typeSpeed:150,
+backSpeed:100,
+backDelay:100,
+loop:true
+});
